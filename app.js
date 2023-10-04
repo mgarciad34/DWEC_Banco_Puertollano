73b9ae3c8da0822a23341d370ed28d7c5de3ddc2
Manuel García <145276674+mgarciad34@users.noreply.github.com>
2023-10-04 21:28:12 +0200

    Estructura creada implementando funcionalidad

--- app.js
+++ app.js
@@ -17,11 +17,14 @@ function cargarCabecera(dest){
 3.1) Cada cuenta puede tener tarjetas de crédito asociadas, las tarjetas están compuestas del 
 número de tarjeta y si están activas o no. Esta tabla es dinámica, cuando el usuario guarde una 
 tarjeta el contenido de la tabla debe registrar la nueva tarjeta.
-*/
 
-
-
-/*3.2) Inicialmente se deben tener cargadas las tarjetas que aparecen por defecto, el campo 
+3.2) Inicialmente se deben tener cargadas las tarjetas que aparecen por defecto, el campo 
 número tarjeta en el apartado de agregar debe seguir el mismo patrón que el mostrado 
 anteriormente. El campo CVV debe contener solo 3 dígitos
 */
+
+function comprobarTarjeta() {
+    const expresionRegular = /^[0-9]{16}$/;
+    return expresionRegular.test(tarjeta.value);
+}
+
