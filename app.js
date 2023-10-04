d56f53a19af0ac0011191fb1bcbbb4d1e973e3e6
Manuel García <145276674+mgarciad34@users.noreply.github.com>
2023-10-04 20:10:23 +0200

    Actualizacion estructura y contenido actividad 3

--- app.js
+++ app.js
@@ -13,3 +13,15 @@ function cargarCabecera(dest){
  document.getElementById(dest).innerHTML = '   <h1>BancoPuertollano</h1>    <ul>        <li><a href="index.html">Inicio</a></li>        <li><a href="infoCuenta.html">Informaci&#243;n Cuenta</a></li>             <li><a href="tarjetas.html">Tarjetas</a></li>    </ul>' 
 }
 
+/*
+3.1) Cada cuenta puede tener tarjetas de crédito asociadas, las tarjetas están compuestas del 
+número de tarjeta y si están activas o no. Esta tabla es dinámica, cuando el usuario guarde una 
+tarjeta el contenido de la tabla debe registrar la nueva tarjeta.
+*/
+
+
+
+/*3.2) Inicialmente se deben tener cargadas las tarjetas que aparecen por defecto, el campo 
+número tarjeta en el apartado de agregar debe seguir el mismo patrón que el mostrado 
+anteriormente. El campo CVV debe contener solo 3 dígitos
+*/
