264c72ee85d5bf21d01f9236a023c7a411eb6926
Manuel García <145276674+mgarciad34@users.noreply.github.com>
2023-10-05 21:48:55 +0200

    Ejercicio 2 solucionado

--- app.js
+++ app.js
@@ -22,9 +22,11 @@ correctamente.
 
 const iban = document.getElementById("txtiban");
 const saldoFijo = document.getElementById("txtsaldo");
-const cantidadRetirar = document.getElementById("txtretirar");
-const cantidadIngresar = document.getElementById("txtingresar");
+var cantidadRetirar = document.getElementById("txtretirar");
+var cantidadIngresar = document.getElementById("txtingresar");
 const mensaje = document.getElementById("mensaje");
+
+
 
 function retirarDinero(){
     const cantidadRetiro = parseFloat(cantidadRetirar.value);
@@ -56,7 +58,8 @@ mostrará un mensaje en verde de que el dinero ha sido ingresado correctamente.
 function ingresarDinero() {
     const cantidadIngreso = parseFloat(cantidadIngresar.value);
     const saldo = parseFloat(saldoFijo.value);
-    const mensaje = document.getElementById("mensaje");
+    // Elimina la siguiente línea, ya que 'mensaje' ya se ha declarado al principio
+    // const mensaje = document.getElementById("mensaje");
 
     if (isNaN(cantidadIngreso) || cantidadIngreso <= 0) {
         mensaje.innerText = "Cantidad no válida.";
@@ -73,77 +76,13 @@ function ingresarDinero() {
         cantidadRetirar.value = "";
     }
 }
-class Persona {
-
-    constructor(nombre, apellido1, apellido2, nacionalidad) {
-      this._nombre = nombre;
-      this._apellido1 = apellido1;
-      this._apellido2 = apellido2;
-      this._nacionalidad = nacionalidad;
-    }
-}
 
 
-/*
-1.1) El botón Modificar Datos en caso de éxito debe volcar la información de al objeto persona
-y mostrar un mensaje en la parte inferior “Guardado los datos correctamente” en color
-verde
-*/
-
-const nombre = document.getElementById('txtname');
-const apellido1 = document.getElementById('txtsubnameone')
-const apellido2 = document.getElementById('txtsubnametwo')
-const nacionalidad = document.getElementById('txtnacionalidad');
-var contadorNumeros = 0;
-
-nombre.addEventListener('change', function() { 
-    texto(nombre , /^[A-Za-z]{3,20}$/); 
-});
-
-apellido1.addEventListener('change', function() { 
-    texto(apellido1 ,/^[A-Za-z]{3,20}$/); 
-});
-
-apellido2.addEventListener('change', function() { 
-    texto(apellido2 ,/^[A-Za-z]{3,20}$/); 
-});
-
-nacionalidad.addEventListener('change', function() { 
-    texto(nacionalidad, /^[A-Za-z]{3,15}$/); 
-});
-
-function texto(inputElement, regEx) {
-    if (regEx.test(inputElement.value)) {
-        inputElement.style.color = "green";
-        contadorNumeros++;
-        return true;
-    } else {
-        inputElement.style.color = "red";
-        return false;
-    }
-}
-
-//Creamos un Array de personas para guardarlas
-const personas = []
-
-function guardarUsuarios() {
-    var mensaje = document.getElementById('mensaje'); // Obtén el elemento donde mostrarás el mensaje
-    
-    if (contadorNumeros === 4) {
-        const nuevaPersona = new Persona(nombre.value, apellido1.value, apellido2.value, nacionalidad.value);
-        personas.push(nuevaPersona);
-        mensaje.textContent = "Guardado los datos correctamente"; // Actualiza el texto del mensaje
-        mensaje.style.color = "green"; // Aplica el estilo al mensaje
-        contadorNumeros = 0;
-        console.log(personas);
-    } else {
-        mensaje.textContent = "No se han guardado los datos"; // Actualiza el texto del mensaje
-        mensaje.style.color = "red"; // Aplica el estilo al mensaje
-        contadorNumeros = 0;
-    }
-
-}
-    
-
-}
+cantidadRetirar.addEventListener("focus", function() {
+      cantidadIngresar.value = "";
+  });
+  
+  cantidadIngresar.addEventListener("focus", function() {
+      cantidadRetirar.value = "";
+  });
 
